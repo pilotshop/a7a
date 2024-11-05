2e1426a4c40dacd5b8f2efced8d9d056d91ba8a2
pilotshop <abdallhshref28@gmail.com>
2024-11-05 22:41:55 +0200

    d

--- api/settings.py
+++ api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4wf7rclc2j+n+=cfa4p1s*sp!r^#=ul3nf0v=9!p6@#b7-jhaw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1','api-five-red.vercel.app']
 
 
 # Application definition
